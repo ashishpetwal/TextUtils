3b1da3cfd63b05d5902b984ae38c1c5931c279f5
Ashish <ashishpetwalap2112@gmail.com>
2023-01-04 14:14:56 +0530

    Added Switch to Toggle Full Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import "./App.css";
-import FAQs from "./components/FAQs";
+// import FAQs from "./components/FAQs";
 import Navbar from "./components/Navbar";
 import TextArea from "./components/TextArea";
+import React, {useState} from "react";
 
 function App() {
+  const[mode, setMode] = useState("light");
+  
+  const toggleBtn = ()=>{
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#021837";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
   return (
     <div>
-      <Navbar title="TextUtils" aboutText="About Us" />
+      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleBtn={toggleBtn}/>
       <div className="container my-5">
-        <TextArea heading="Enter Your Text"/>
+        <TextArea heading="Enter Your Text" mode={mode}/>
       </div>
-      <div className="container">
+      {/* <div className="container">
         <FAQs/>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -12,20 +12,24 @@ export default function TextArea(props) {
         let temp = text.toLowerCase();
         newText(temp);
     }
+    let style = {
+        backgroundColor: props.mode === "light"?"white":"#00141e",
+        color: props.mode === "light"?"black":"white"
+    };
     const[text, newText] = useState("Type your Paragraph");
     return (
         <>
         <div className="container">
-            <h2>{props.heading}</h2>
+            <h2 style={{color: props.mode === "dark"?"white":"black"}}>{props.heading}</h2>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="onlyBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} style={style} onChange={handleOnChange} id="onlyBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpperCase}>Convert UpperCase</button> 
             <button className="btn btn-success mx-3" onClick={handleLowerCase}>Convert LowerCase</button> 
         </div>
         <div className="container my-3">
-            <h2>Text Summary</h2><hr></hr>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <h2 style={{color: props.mode === "dark"?"white":"black"}}>Text Summary</h2><hr></hr>
+            <p style={{color: props.mode === "dark"?"white":"black"}}>{text.split(" ").length} words and {text.length} characters</p>
         </div>
         </>
     );
