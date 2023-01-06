d6eaf78cde126e9a36c204f36ad2c13dde793ee3
Ashish <ashishpetwalap2112@gmail.com>
2023-01-06 22:52:50 +0530

    Added React Router Dom in App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-// import FAQs from "./components/FAQs";
+import FAQs from "./components/FAQs";
 import Navbar from "./components/Navbar";
 import TextArea from "./components/TextArea";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
-  const[mode, setMode] = useState("light");
-  const[alert, setAlert] = useState(null);
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (alertMsg, type)=>{
+  const showAlert = (alertMsg, type) => {
     setAlert({
       alertMsg: alertMsg,
       type: type
@@ -18,14 +19,14 @@ function App() {
       setAlert(null);
     }, 1500);
   }
-  
-  const toggleBtn = ()=>{
-    if(mode === "light"){
+
+  const toggleBtn = () => {
+    if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#021837";
       showAlert("Dark Mode is Enabled", "success");
     }
-    else{
+    else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light Mode is Enabled", "success");
@@ -33,16 +34,14 @@ function App() {
   }
 
   return (
-    <div>
-      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleBtn={toggleBtn}/>
-      <Alert alert={alert}/>
-      <div className="container my-5">
-        <TextArea heading="Enter Your Text" mode={mode} showAlert={showAlert}/>
-      </div>
-      {/* <div className="container">
-        <FAQs/>
-      </div> */}
-    </div>
+    <Router>
+      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleBtn={toggleBtn} />
+      <Alert alert={alert} />
+      <Routes>
+        <Route path="/" element={<div className="my-5"><TextArea heading="Enter Your Text" mode={mode} showAlert={showAlert} /></div>}></Route>
+        <Route path="/faqs" element={<FAQs/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
